67aef2ea6a31de6f4c0b200676d7b8a7afb2a75d
CCTsang <cctsangofficial@gmail.com>
2022-10-20 17:26:50 +0800

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,50 +1,63 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, List } from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem, List
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-    function RenderDish({dish}) {
-        return (
-            <Card>
-                <CardImg top src={dish.image} alt={this.props.selectedDish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        )
-    }
+function RenderDish({ dish }) {
+    return (
+        <div>
+        <Card>
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+        </div>
+    )
+}
 
-    function RenderComments({comment}) {
-        return (
-            comment.map((comments) => {
-                return (
-                    <li key={comments.id}>
-                        <p>{comments.comment}</p>
-                        <p>-- {comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comments.date)))}</p>
-                    </li>
-                )
-            })
-        )
-    }
+function RenderComments({ comments }) {
+    return (
+        comments.map((comment) => {
+            return (
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                </li>
+            )
+        })
+    )
+}
 
 
-    const DishDetail = (props) => {
-        return (
-            <div className='container'>
-            <div className='row'>
-                <div className='col-12 col-md-5 m-1'>
-                    <RenderDish dish={props.selectedDish} />
-                </div>
-                <div className='col-12 col-md-5 m-1'>
-                    <h4>Comments</h4>
-                    <List type='unstyled'>
-                        <RenderComments comments={props.selectedDish.comments} />
-                    </List>
-                </div>
+const DishDetail = (props) => {
+    return (
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
             </div>
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
             </div>
-        )
-    }
+        </div>
+        </div>
+    );
+}
 
 
 export default DishDetail;
